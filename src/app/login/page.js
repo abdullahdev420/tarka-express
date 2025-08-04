7f461de127759b0b113365abdc94093880cf8b0d
Abdullah Shah <shah.abdullahdev@gmail.com>
2025-08-04 17:02:29 +0500

    Fixed bugs and ready for Netlify deployment

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -107,7 +107,7 @@ export default function LoginPage() {
           )}
 
           <p className="text-sm text-center mt-4">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <a href="/signup" className="text-blue-600 hover:underline">
               Sign up
             </a>
